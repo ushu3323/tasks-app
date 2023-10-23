0b023f701b24bbd7e61eb68c570b456f2335ddf1
ushu3323 <redspower10@gmail.com>
2023-10-23 18:51:22 -0300

    Implement semantic html in tasks list

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -7,9 +7,9 @@ interface Props {
 
 export default function TasksList({ tasks, onDelete }: Props) {
   return (
-    <div className="vstack gap-2">
+    <section id="tasks-list" className="vstack gap-2">
       {tasks.map(task => (
-        <div key={task.id} className="card">
+        <article key={task.id} className="card">
           <div className="card-body d-flex">
             <p className="m-0 me-2 flex-fill d-inline">
               <span className="align-middle">{task.content}</span>
@@ -17,11 +17,11 @@ export default function TasksList({ tasks, onDelete }: Props) {
             <button
               className="btn btn-danger align-self-start"
               onClick={() => onDelete && onDelete(task.id)}>
-              <i className="bi bi-trash-fill"></i>
+              <i className="bi bi-trash-fill" aria-hidden="true"></i>
             </button>
           </div>
-        </div>
+        </article>
       ))}
-    </div>
+    </section>
   );
 }
