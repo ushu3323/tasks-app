fca24e4488cd41d8dcdc42fd1d94f0a850b67529
ushu3323 <redspower10@gmail.com>
2023-10-23 18:52:19 -0300

    Remove react fragment in App component

--- src/App.tsx
+++ src/App.tsx
@@ -41,28 +41,23 @@ function App() {
   }
 
   return (
-    <>
-      <div className="container py-4 px-3 mx-auto">
-        <h1>Tasks app</h1>
-        <form
-          onSubmit={handleSubmit}
-          className="d-flex my-4"
-          autoComplete="off">
-          <input
-            name="content"
-            type="text"
-            className="form-control me-2"
-            placeholder="Write your task here..."
-            required
-            onChange={e => setTaskContent(e.currentTarget.value)}
-          />
-          <button type="submit" className="btn btn-primary">
-            Añadir
-          </button>
-        </form>
-        <TasksList tasks={tasks} onDelete={deleteTask} />
-      </div>
-    </>
+    <div className="container py-4 px-3 mx-auto">
+      <h1>Tasks app</h1>
+      <form onSubmit={handleSubmit} className="d-flex my-4" autoComplete="off">
+        <input
+          name="content"
+          type="text"
+          className="form-control me-2"
+          placeholder="Write your task here..."
+          required
+          onChange={e => setTaskContent(e.currentTarget.value)}
+        />
+        <button type="submit" className="btn btn-primary">
+          Añadir
+        </button>
+      </form>
+      <TasksList tasks={tasks} onDelete={deleteTask} />
+    </div>
   );
 }
 
