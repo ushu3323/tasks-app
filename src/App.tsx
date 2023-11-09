ba39e92eba2a38193ef0dbec254ed4668c27e162
ushu3323 <redspower10@gmail.com>
2023-11-09 20:39:56 -0300

    Set a fixed width for tasks list

--- src/App.tsx
+++ src/App.tsx
@@ -14,26 +14,31 @@ function App() {
   }
 
   return (
-    <div className="container py-4 px-3 mx-auto d-flex flex-column min-vh-100">
-      <h1>Tasks app</h1>
-      <form onSubmit={handleSubmit} className="d-flex my-4" autoComplete="off">
-        <input
-          name="content"
-          type="text"
-          className="form-control me-2"
-          placeholder="Write your task here..."
-          required
-          onChange={e => setTaskContent(e.currentTarget.value)}
+    <div className="container-lg py-4 px-3 mx-auto d-flex flex-column min-vh-100">
+      <div className="mx-auto col-12">
+        <h1>Tasks app</h1>
+        <form
+          onSubmit={handleSubmit}
+          className="d-flex my-4"
+          autoComplete="off">
+          <input
+            name="content"
+            type="text"
+            className="form-control me-2"
+            placeholder="Write your task here..."
+            required
+            onChange={e => setTaskContent(e.currentTarget.value)}
+          />
+          <button type="submit" className="btn btn-primary">
+            Add
+          </button>
+        </form>
+        <TasksList
+          tasks={tasksService.tasks}
+          onTaskDelete={tasksService.remove}
+          onTaskEdit={tasksService.update}
         />
-        <button type="submit" className="btn btn-primary">
-          Add
-        </button>
-      </form>
-      <TasksList
-        tasks={tasksService.tasks}
-        onTaskDelete={tasksService.remove}
-        onTaskEdit={tasksService.update}
-      />
+      </div>
     </div>
   );
 }
